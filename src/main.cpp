254018f41290c029b6de7ba0633e0a88ed765295
Arvin Singh <29691465+arvinsingh@users.noreply.github.com>
2024-06-18 18:22:51 +0100

    added: main

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: Arvin Singh
+ * Date: 01/04/2024
+ * Description: Main entry point for the application.
+*/
+
+
+#include "todo.h"
+
+int main(int argc, char *argv[]) { return App::run(argc, argv); }
